390633e4d32d9983a701468fab77f2369ead1062
arzWZM <arzWZM@protonmail.com>
2021-08-25 21:58:39 +0530

    Updated SCM and dist

---
+++ src/test/java/com/example/devops/WelcomeControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.devops;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+//@RunWith(SpringJunit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class WelcomeControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	@Before
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+	
+	@Test
+	public void testWelcome() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk());
+		
+	}
+
+
+}
