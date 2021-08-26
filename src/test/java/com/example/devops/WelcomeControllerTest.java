b8397e4aae06a1fee34156e7ee8c9bc33c7aa6ad
arzWZM <arzWZM@protonmail.com>
2021-08-26 15:20:23 +0530

    Fixed issue with junit and updated junit version

--- src/test/java/com/example/devops/WelcomeControllerTest.java
+++ src/test/java/com/example/devops/WelcomeControllerTest.java
@@ -7,17 +7,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 //@RunWith(SpringJunit4ClassRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class WelcomeControllerTest {
 
